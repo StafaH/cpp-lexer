7f2c083f7002b1cbce39903f393cf1f07c24a4e5
StafaH <mhaiderbhai@hotmail.com>
2018-12-01 23:18:07 -0500

    Modify TODO and other comments

--- code/main.cpp
+++ code/main.cpp
@@ -12,7 +12,21 @@ data at runtime.
 e.g. 
 change x = 5	-> variable x assigned value of 5
 list variables	-> list all variables that can be changed
+
+TODO: too verbose?
+New ideas:
+output x 
+x = 5
+ls listname - default?
+
+TODO: Implement type storage, check if types match behind the scenes
+to avoid user typing out types
 */
+
+//TODO: Another implementation might be to use pointers into the original string,
+// this will eliminate the overhead of copying + storing, as long as we hold the original
+// in memory, otherwise the tokens become invalid 
+
 
 int main()
 {
@@ -24,7 +38,9 @@ int main()
 	char my_str[256] = "Test String";
 	int another_int = 7;
 	int my_int = 5;
-
+	
+	//TODO: abstract this, how do we add new variables, and do cool stuff with
+	// the variables already in there other than change/list
 	TokenDictionaryItem token_dict[] =
 		{
 			{VarToString(my_int), &my_int, "int"},
